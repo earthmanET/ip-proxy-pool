9d7b47416b5f88f3cd0bc5f400ba010e0b149c7d
AmI Who <jsdsj13@yeah.net>
2018-09-16 16:52:27 +0800

    first

---
+++ get_proxy_ip.py
@@ -0,0 +1,108 @@
+#coding=utf-8
+
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import csv
+
+def xici_ip_spider(start,end):
+    url='http://www.xicidaili.com/nn/'
+    csvfile=file('xici.csv','w')
+    writer=csv.writer(csvfile)
+    print("The result will be saved in xici.csv")
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+    for num in xrange(start,end+1):
+        sleep(2)
+        print('Now downloaing the page' + str(num) + ' ips')
+        request=requests.get(url+str(num),headers=headers)
+        content=request.text
+
+        soup = BeautifulSoup(content, 'html.parser')
+        trs=soup.find_all('tr')
+        for item in trs:
+            try:
+                temp=[]
+                tds=item.find_all('td')
+                temp.append(tds[1].text.encode('utf-8'))
+                temp.append(tds[2].text.encode('utf-8'))
+                temp.append(tds[5].text.encode('utf-8'))
+                writer.writerow(temp)
+            except IndexError:
+                pass
+
+def kuaidaili_ip_spider(start,end):
+    url='https://www.kuaidaili.com/free/inha/'
+    csvfile=file('kuaidaili.csv','w')
+    print("The result will be saved in kuaidaili.csv")
+    writer=csv.writer(csvfile)
+    s = requests.session()
+    s.keep_alive = False
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+             'Connection': 'close'
+             }
+    for num in xrange(start,end+1):
+        sleep(2)
+        print('Now downloa ing the page' + str(num) + ' ips')
+        try:
+            request=requests.get(url+str(num),headers=headers,timeout=12)
+        except Exception as e:
+            print(e)
+            pass
+        content=request.text
+        soup=BeautifulSoup(content,'html.parser')
+        trs=soup.find_all('tr')
+        for item in trs:
+            try:
+                temp=[]
+                tds=item.find_all('td')
+                temp.append(tds[0].text.encode('utf-8'))
+                temp.append(tds[1].text.encode('utf-8'))
+                temp.append(tds[3].text.encode('utf-8'))
+                writer.writerow(temp)
+            except IndexError:
+                pass
+
+def liuliuip_ip_spider(start,end):
+    url='http://www.66ip.cn/'
+    csvfile=file('66ip.csv','w')
+    print("The result will be saved in 66ip.csv")
+    writer=csv.writer(csvfile)
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+    for num in xrange(start,end+1):
+        sleep(2)
+        print('Now downloaing the page' + str(num) + ' ips')
+        try:
+            request=requests.get(url+str(num)+'.html',headers=headers,timeout=12)
+        except Exception as e:
+            pass
+        content=request.text
+        soup=BeautifulSoup(content,'html.parser')
+        trs=list(soup.find_all('tr'))
+        for item in trs[2:]:
+            try:
+                temp=[]
+                tds=item.find_all('td')
+                temp.append(tds[0].text.encode('utf-8'))
+                temp.append(tds[1].text.encode('utf-8'))
+                writer.writerow(temp)
+            except IndexError:
+                pass
+
+
+def main():
+    print("Please select the website you want to crawl:")
+    print("[1]www.xicidaili.com")
+    print("[2]www.kuaidaili.com")
+    print("[3]www.66ip.cn")
+    answer=input()
+    if answer==1:
+        xici_ip_spider(1,100) #可手动更改页数
+    elif answer==2:
+        kuaidaili_ip_spider(1,100)
+    elif answer==3:
+        liuliuip_ip_spider(1,100)
+    else:
+        exit()
+
+if __name__ == '__main__':
+    main()
---
+++ test_proxy_ip.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+
+import requests
+import sys
+import threading
+from time import sleep
+import csv
+
+csvfile=open('valid_ip.csv','w')
+writer=csv.writer(csvfile)
+
+class test_thread(threading.Thread):
+    def __init__(self,ip,port):
+        threading.Thread.__init__(self)
+        self.ip=ip
+        self.port=port
+        #self.protocol=protocol
+    def run(self):
+        s = requests.session()
+        s.keep_alive = False
+        requests.adapters.DEFAULT_RETRIES = 5
+        proxies = {"http": "http://"+self.ip + ":" + str(self.port)}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+            'Connection': 'close'
+        }
+        try:
+            maskedIP = str(requests.get("http://icanhazip.com/", headers=headers, timeout=8, proxies=proxies).text).replace("\n","")
+        except Exception as e:
+            return
+        if maskedIP == self.ip:
+            writer.writerow([self.ip, self.port])
+            print(self.ip+" "+self.port)
+
+def test_proxy(filename):
+    reader=csv.reader(open(filename))
+    for row in reader:
+        ip, port=row[0],row[1]
+        while threading.activeCount() >30:
+            sleep(2)
+        test_thread(ip,port).start()
+
+def main():
+    filename=sys.argv[1]
+    print("The result will be saved in valid_ip.csv")
+    test_proxy(filename)
+
+if __name__ == '__main__':
+    main()
